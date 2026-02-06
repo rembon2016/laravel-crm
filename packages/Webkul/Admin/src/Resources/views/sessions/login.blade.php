b696ce16e63e7269a1ee2e5024f9fdd12b0c1cb7
dayCod <alayarahman07@gmail.com>
2026-02-06 12:18:13 +0700

    fix: update powered-by descriptions and links in multiple files

--- packages/Webkul/Admin/src/Resources/views/sessions/login.blade.php
+++ packages/Webkul/Admin/src/Resources/views/sessions/login.blade.php
@@ -104,10 +104,7 @@
 
         <!-- Powered By -->
         <div class="text-sm font-normal">
-            @lang('admin::app.components.layouts.powered-by.description', [
-                'krayin' => '<a class="text-brandColor hover:underline " href="https://krayincrm.com/">Krayin</a>',
-                'webkul' => '<a class="text-brandColor hover:underline " href="https://webkul.com/">Webkul</a>',
-            ]) 
+            @lang('admin::app.components.layouts.powered-by.description')
         </div>
     </div>
 
